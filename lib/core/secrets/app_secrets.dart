d23a166917373fc0c896688eef71e32fc1ab3c58
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-07-10 21:56:23 +0200

    feat : application secrets setup

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const String supabaseUrl = "https://ibitymxvjewgisbeoldp.supabase.co";
+  static const String supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliaXR5bXh2amV3Z2lzYmVvbGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNzUyNzcsImV4cCI6MjA2Nzc1MTI3N30.eg4SPiv-JLcfiB0TWlxJ5NsIBAXFOUG_ga2FAs5urq8";
+}
