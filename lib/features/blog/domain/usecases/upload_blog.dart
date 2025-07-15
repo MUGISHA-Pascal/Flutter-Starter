4a0eb9d518632e008f3c181c030300819e3eae92
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-07-15 14:45:08 +0200

    fix : correct import for usecase file

--- lib/features/blog/domain/usecases/upload_blog.dart
+++ lib/features/blog/domain/usecases/upload_blog.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 import 'package:flutter_starter/core/error/failures.dart';
 import 'package:flutter_starter/core/usecase/usecase.dart';
+import 'package:flutter_starter/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:flutter_starter/features/blog/domain/entities/blog.dart';
-import 'package:flutter_starter/features/blog/domain/repositories/blog_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
 class UploadBlog implements Usecase<Blog, UploadBlogParams> {
-  final BlogRepository blogRepository;
+  final BlogRepositoryImpl blogRepository;
   UploadBlog(this.blogRepository);
 
   @override
